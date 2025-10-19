67cd9b49e81e914daeddd958b805a331c420bffc
postfm <72159645+postfm@users.noreply.github.com>
2025-10-19 18:40:27 +0300

    feat: Добавит команды в package.json для работы с БД.

---
+++ src/prisma/seed.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from '@prisma/client';
+import { fakerDE as faker } from '@faker-js/faker';
+
+const INIT_COUNT_VALUE = 0;
+const COUNT_VALUE = 20;
+
+function getUsers() {
+  const emails = faker.helpers.uniqueArray(faker.internet.email, COUNT_VALUE);
+  const users = [];
+  for (let i = INIT_COUNT_VALUE; i < COUNT_VALUE; i++) {
+    users.push({
+      id: faker.string.uuid(),
+      name: faker.person.fullName(),
+      email: emails[i],
+      password: faker.internet.password(),
+      role: faker.helpers.arrayElement(['ADMIN', 'USER']),
+      status: faker.helpers.arrayElement(['Active', 'Not Active']),
+    });
+  }
+  return users;
+}
+
+async function seedDb(prismaClient: PrismaClient) {
+  const mockUsers = getUsers();
+  for (const user of mockUsers) {
+    await prismaClient.user.upsert({
+      where: { id: user.id },
+      update: {},
+      create: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        role: user.role,
+        status: user.status,
+      },
+    });
+  }
+
+  console.info('🤘️ Database was filled');
+}
+
+async function bootstrap() {
+  const prismaClient = new PrismaClient();
+
+  try {
+    await seedDb(prismaClient);
+    globalThis.process.exit(0);
+  } catch (error: unknown) {
+    console.error(error);
+    globalThis.process.exit(1);
+  } finally {
+    await prismaClient.$disconnect();
+  }
+}
+
+bootstrap();
